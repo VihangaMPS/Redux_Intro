e7f415e2b2f0ed1e4246a48a4070ef756286a87f
MPS Vihanga <>
2024-04-13 11:57:50 +0530

    using (BalanceDisplay.js) component to showcase connecting Classic Redux

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
     return (
         <div>
-            <h1>🏦 The React-Redux Bank ⚛️</h1>
+            <h1>🏦 The Redux Bank 🏦</h1>
             {fullName === '' ?
                 <CreateCustomer/> :
                 <>
