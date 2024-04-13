35d8554a16b89c5d57285d1e0f414cf286787604
MPS Vihanga <>
2024-04-14 01:42:44 +0530

    making an API call with Redux Thunks & updated BalanceDisplay.js file

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
+import {thunk} from "redux-thunk";
 
 
 const rootReducer = combineReducers({
@@ -9,7 +10,7 @@ const rootReducer = combineReducers({
 })
 
 //Redux Config
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-export default store;
+export default store; 
 
